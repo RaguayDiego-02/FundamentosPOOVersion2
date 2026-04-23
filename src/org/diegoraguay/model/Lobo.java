edebce1c46743e8419b22e4f6eec115de5e90da8
Diego Raguay <draguay-2026067@kinal.edu.gt>
2026-04-22 20:01:59 -0600

    010 Se creo la subclase Oso y se guardo un cambio de la clase Lobo

--- src/org/diegoraguay/model/Lobo.java
+++ src/org/diegoraguay/model/Lobo.java
@@ -3,6 +3,15 @@ package org.diegoraguay.model;
 public class Lobo extends AnimalesSalvajes{
     private boolean vaEnManada;
     
+    public void aullar(){
+    System.out.println("El lobo a aullado");
+    }
     
+    public boolean getvaEnManada(){
+    return this.vaEnManada; 
+    }
     
+    public void setvaEnManada(boolean vaEnManada){
+    this.vaEnManada = vaEnManada; 
+    }
 }
